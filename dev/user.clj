5f0348a3fbfe41485de8825e2fdfdd375ce39a4d
Christopher Small <metasoarous@gmail.com>
2022-09-03 22:45:01 -0700

    splitting python requires to see where it's falling on its face

--- dev/user.clj
+++ dev/user.clj
@@ -1,13 +1,14 @@
 
 (ns user
   (:require [tech.v3.dataset :as ds]
-            [tech.v3.datatype :as dt]))
-            ;[libpython-test.core]))
+            [tech.v3.datatype :as dt]
+            [libpython-test.test :as test]))
+
             ;[libpython-clj2.require :refer [require-python]]
             ;[libpython-clj2.python :as py :refer [py. py.. py.-]]))
 
 (try
-  (require 'libpython-test.core)
+  (println "I have come round:" (test/full-circle))
   (catch Throwable t
     (.printStackTrace t)))
 
--- src/libpython_test/test.clj
+++ src/libpython_test/test.clj
@@ -1,9 +1,32 @@
 (ns libpython-test.test
-  (:require [libpython-clj2.python :as py]))
+  (:require [libpython-clj2.python :as py]
+            [libpython-clj2.require :refer [require-python import-python]]))
 
+(println "Establishing python link")
 (py/initialize!)
+(import-python)
+(println "Link established; requiring libs...")
+(require-python '[sklearn.datasets :as sk-data])
+(println "  requiring sk-data...")
+(require-python '[sklearn.model_selection :as sk-model])
+(println "  requiring sk-decomp...")
+(require-python '[sklearn.decomposition :as sk-decomp])
+(println "  requiring numpy...")
+(require-python '[numpy :as numpy])
+(println "  requiring numba...")
+(require-python '[numba :as numba])
+(println "  requiring pandas...")
+(require-python '[pandas :as pandas])
+(println "  requiring umap...")
+(require-python '[umap :as umap])
+(println "  requiring umap_metric...")
+(require-python '[umap_metric :as umap_metric :reload])
+(println "DONE with python requires")
 
-;(require '[libpython-clj2.require :as py-req])
-;(require 'libpython-clj2.metadata)
+(defn full-circle []
+  (py/->jvm (py/->python (repeatedly 10 #(rand-int 100)))))
+
+;(full-circle)
+
 
 (println :OK)
